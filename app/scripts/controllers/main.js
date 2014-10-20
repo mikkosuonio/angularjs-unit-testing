48901014c2dcb02af907493a3438796723f97969
Mikko Suonio <mikko.suonio@solita.fi>
2014-10-20 16:06:27 +0300

    Show things in view

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,8 +10,17 @@
 angular.module('jsunittestingApp')
   .controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+      {
+        'name': 'HTML5 Boilerplate',
+        'description': 'HTML5 Boilerplate is a professional front-end template for building fast, robust, and adaptable web apps or sites.'
+      },
+      {
+        'name': 'AngularJS',
+        'description': 'AngularJS is a toolset for building the framework most suited to your application development.'
+      },
+      {
+        'name': 'Karma',
+        'description': 'Spectacular Test Runner for JavaScript.'
+      }
     ];
   });
